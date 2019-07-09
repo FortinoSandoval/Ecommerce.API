c510c8c49febe62b597059b5b995cbd4e0071543
Fortino Sandoval <Fortino.Sandoval@maritz.com>
2019-07-09 14:01:39 -0700

    .

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Ecommerce.API
+
+*Add this to `main/resources/application.properties` and change password and username* 
+```ring.datasource.driverClassName=com.microsoft.sqlserver.jdbc.SQLServerDriver
+spring.datasource.url=jdbc:sqlserver://localhost;databaseName=ecommerce
+spring.datasource.username=USERNAME
+spring.datasource.password=PASSWORD
+spring.jpa.show-sql=true
+spring.jpa.properties.hibernate.format_sql = true
+spring.jpa.properties.hibernate.dialect = org.hibernate.dialect.SQLServer2012Dialect
+spring.jpa.hibernate.ddl-auto = update
+server.servlet.context-path=/api
+jwt.secret=THISISA"V3RY"S3CRET$STRING*!
--- src/main/java/com/fortinosandoval/ecommerceapi/controllers/EmployeeController.java
+++ src/main/java/com/fortinosandoval/ecommerceapi/controllers/EmployeeController.java
@@ -14,14 +14,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fortinosandoval.ecommerceapi.models.entity.Employee;
 import com.fortinosandoval.ecommerceapi.repository.EmployeeRepository;;
 
 @RestController
-@RequestMapping("/api")
 public class EmployeeController {
   @Autowired
   private EmployeeRepository employeeRepository;
