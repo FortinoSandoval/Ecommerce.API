155e7dae16e2624f7c90f6923fb5cef6375c52a7
Fortino Sandoval <Fortino.Sandoval@maritz.com>
2019-07-10 07:46:07 -0700

    swagger added

--- src/main/java/com/fortinosandoval/ecommerceapi/exception/ResourceNotFoundException.java
+++ src/main/java/com/fortinosandoval/ecommerceapi/exception/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package com.fortinosandoval.ecommerceapi.controllers;
+package com.fortinosandoval.ecommerceapi.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
