52af68f9583a650f63ac672c19cd0d6293aa5fd3
Fortino Sandoval <Fortino.Sandoval@maritz.com>
2019-07-10 12:17:47 -0700

    removed unused imports

--- src/main/java/com/fortinosandoval/ecommerceapi/controllers/JwtAuthenticationController.java
+++ src/main/java/com/fortinosandoval/ecommerceapi/controllers/JwtAuthenticationController.java
@@ -1,7 +1,5 @@
 package com.fortinosandoval.ecommerceapi.controllers;
 
-import java.util.Objects;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
